4d6438cdfcbc5b477271c7c269515dcad1989fb4
AscentLe <35le.mathieu@gmail.com>
2024-10-09 09:30:59 +0200

    Allocation function

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+typedef struct HEADER_TAG {
+    struct HEADER_TAG * ptr_next;
+    size_t bloc_size;
+    long magic_number; /* 0x0123456789ABCDEFL;*/ 
+}HEADER;
+
+void* malloc_3is(size_t size){
+    int *currentBreak = sbrk(size + 2*sizeof(long));
+    if (currentBreak == (void *) -1){
+        if (errno==ENOMEM){
+            printf("Allocation error/Not enough space");
+        }
+    }
+    return NULL;
+}
+
+void free_3is(void *ptr) {
+
+}
+
+int main(){
+    malloc_3is(10);
+}
