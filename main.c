a13e43e8d93f02c2c9ce5af481ad1e4fda11635b
AscentLe <35le.mathieu@gmail.com>
2024-10-09 10:13:10 +0200

    Test for malloc

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+#define MAGIC_NUMBER 0x0123456789ABCDEFL
 typedef struct HEADER_TAG {
     struct HEADER_TAG * ptr_next;
     size_t bloc_size;
@@ -15,19 +16,33 @@ typedef struct HEADER_TAG {
 }HEADER;
 
 void* malloc_3is(size_t size){
-    int *currentBreak = sbrk(size + sizeof(HEADER) + sizeof(long));
+    void *currentBreak = sbrk(size + sizeof(HEADER) + sizeof(long));
     if (currentBreak == (void *) -1){
         if (errno==ENOMEM){
             printf("Allocation error/Not enough space");
         }
     }
-    return NULL;
+    return currentBreak;
 }
 
-void free_3is(void *ptr) {
-
-}
 
 int main(){
-    malloc_3is(10);
+    printf("size of HEADER : %ld\n", sizeof(HEADER));
+    void *currentBreak = sbrk(0);
+    currentBreak = sbrk(10);
+    printf("First adress : %p\n", currentBreak);
+    currentBreak = malloc_3is(10);
+    printf("Adress before malloc 10 ; %p\n", currentBreak);
+    currentBreak = malloc_3is(100);
+    printf("Adress before malloc 100 ; %p\n", currentBreak);
+    currentBreak = sbrk(0);
+    printf("Last adress : %p\n", currentBreak);
 }
+
+
+/*void free_3is(void *ptr) {
+    if (ptr == NULL){
+        return;
+    }
+    HEADER* head = 
+}*/
